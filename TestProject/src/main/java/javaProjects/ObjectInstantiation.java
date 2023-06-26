44c69201a6bc806d3ad88bc8d433bb4b605b70af
Pavan <spavan1812@gmail.com>
2023-06-26 15:26:47 +0530

    New changes as on 26 June

---
+++ TestProject/src/main/java/javaProjects/ObjectInstantiation.java
@@ -0,0 +1,41 @@
+package javaProjects;
+  
+
+//AccessModifier  //    ClassName
+public class ObjectInstantiation {
+	
+     //instance Variables	
+	 int id;//field or data member or instance variable  
+	 String name;
+	 
+	 //static variable
+	 static String CollegeName="Badruka";
+	 
+	 //static method
+	 static void methodA(){
+		 System.out.println("this is static method called");
+	 }
+	 
+	 //non static method
+	 void methodB(){
+		 System.out.println("this is non static or mthod B method called");
+	 }
+	 
+	 //creating main method inside the  class  
+	 public static void main(String args[]){  
+		 
+	 //for static references no need of Object reference
+	 //Hence Object instantiation is not needed to call this methods or variables
+		 methodA();
+		 System.out.println(CollegeName);
+		 
+		 //non static References
+    //	Methods and Variables can be called only using Object reference
+		 //Hence called variables are called  instance variables
+		 ObjectInstantiation obj=new ObjectInstantiation();
+		 obj.methodB();
+		 System.out.println(obj.name);
+		  
+	 }  
+
+}
--- TestProject/src/main/java/seleniumPrograms/WebDriverTest_Firefox.java
+++ TestProject/src/main/java/seleniumPrograms/WebDriverTest_Firefox.java
@@ -1,4 +1,4 @@
-package SeleniumMaven.TestProject;
+package seleniumPrograms;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;  
@@ -9,34 +9,32 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;  
 
 
-public class WebDriverTest_Chrome {
+public class WebDriverTest_Firefox {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-
-	    // declaration and instantiation of objects/variables  
-	    System.setProperty("webdriver.chrome.driver", "C:\\Users\\Public\\chromedriver.exe");
-	
-	    WebDriver driver = new ChromeDriver();
+		
+		//new commit using Vs code
+	    System.setProperty("webdriver.firefox.marionette","C:\\\\Users\\\\Public\\\\geckodriver.exe");  
+	    WebDriver driver= new FirefoxDriver();
+	    driver.manage().window().maximize();
 	    
+		
 	    
 		// Launch website  
 	    driver.navigate().to("http://www.google.com/");  
 	          
 	    
 	    Thread.sleep(2000);
-	    
-		//this will maximize window
-	    driver.manage().window().maximize();
 		
 	    // Click on the search text box and send value  
 	    driver.findElement(By.id("APjFqb")).sendKeys("javatpoint tutorials");
 	    
 	        
 	    // Click on the search button  
-	    driver.findElement(By.name("btnK")).click();  
+	    //driver.findElement(By.name("btnK")).click();  
 	    
-	    driver.findElement(By.xpath("//input[contains(@value,'Google Search') and @name='btnK'])[2]")).click(); 
+	    //driver.findElement(By.xpath("//input[contains(@value,'Google Search') and @name='btnK'])[2]")).click(); 
 	    
 	    Thread.sleep(2000);
 	    
